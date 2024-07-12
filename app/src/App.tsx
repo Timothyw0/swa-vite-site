e056dc879c2d819d112a18a2408d254ccb919ad7
Timothy Wang <timothywang@microsoft.com>
2024-07-12 11:57:51 -0400

    Push dedicated change

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p>
-        Hello new change. Hello world! Howdy 🤠! Yippee! Here's a change for dedicated, pls log :(
+        Hello new change. Hello world! Howdy 🤠! Yippee! Here's a change for dedicated again!
       </p>
     </>
   )
