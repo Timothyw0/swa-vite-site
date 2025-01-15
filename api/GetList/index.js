d4d788c599155cfdc4a4b18528d1bc12ae92c02a
Timothy Wang <30346660+Timothyw0@users.noreply.github.com>
2025-01-15 14:10:41 -0500

    Update index.js

--- api/GetList/index.js
+++ api/GetList/index.js
@@ -2,6 +2,6 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.res = {
-        body: ["Jarod", "Hannah", "Kevin", "Mitch","Colby", "Hello world", "New API Change by Tim", "Hello world v20", "change"]
+        body: ["Jarod", "Hannah", "Kevin", "Mitch","Colby", "Hello world", "New API Change by Tim", "Hello world v20", "change2"]
     }
 };
